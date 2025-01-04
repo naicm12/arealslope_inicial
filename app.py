861fea6893232a7022ff00830b3a8569e9315d14
naicm12 <163166528+naicm12@users.noreply.github.com>
2025-01-04 09:35:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.image(img)
 st.markdown("### 1ª Corrida Areal Slope")
 st.write("")
 #st.markdown("### 🔒Inscrições Encerradas")
-
-st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
+st.warning("INSCRIÇÕES ENCERRADAS!)
+#st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
 
 with st.form("Informativo", border=False):
     st.markdown("##### Informativo da Corrida")
@@ -28,8 +28,7 @@ with st.form("Regulamento"):
         lines = f.readlines()
         for line in lines:
             st.write(line)
-    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
-    #st.link_button(label="Clique aqui para realizar sua inscrição",url="http://191.217.246.233:8501/",type="primary")
+    #st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
     st.form_submit_button("",disabled=True)
 
 from PIL import Image
