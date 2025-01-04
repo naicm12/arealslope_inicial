aebf27e359f9021ab9895e0fd7550e6e809f9f84
naicm12 <163166528+naicm12@users.noreply.github.com>
2025-01-04 16:25:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ img = Image.open('02.jpg')
 st.image(img)
 st.markdown("### 1ª Corrida Areal Slope")
 st.write("")
-st.markdown("### 🔒Inscrições Encerradas")
-#st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
+#st.markdown("### 🔒Inscrições Encerradas")
+st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
 
 with st.form("Informativo", border=False):
     st.markdown("##### Informativo da Corrida")
@@ -27,7 +27,7 @@ with st.form("Regulamento"):
         lines = f.readlines()
         for line in lines:
             st.write(line)
-    #st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
+    st.link_button(label="Clique aqui para realizar sua inscrição",url="https://arealslope-inscricao-050956b44d4a.herokuapp.com/",type="primary")
     st.form_submit_button("",disabled=True)
 
 from PIL import Image
